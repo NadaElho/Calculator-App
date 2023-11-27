37e21c659a26d04de5bfa9378404081907625007
NadaElho <nadaelhosary51@gmail.com>
2023-11-27 19:27:52 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+function Heading(){
+  return(
+    <h1> This is an h1 </h1>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
